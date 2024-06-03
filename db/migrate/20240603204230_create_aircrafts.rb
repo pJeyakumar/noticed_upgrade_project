291e8b006f37b615b1bbaaf23b8ce7b590966ef9
Piranavan Jeyakumar <pirana@piranavans-mbp.home>
2024-06-03 16:45:05 -0400

    feat: step 8 running migrations to create tables for users, aircrafts, and logbook_entries

---
+++ db/migrate/20240603204230_create_aircrafts.rb
@@ -0,0 +1,11 @@
+class CreateAircrafts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :aircrafts do |t|
+      t.string :make
+      t.string :model
+      t.integer :engine
+
+      t.timestamps
+    end
+  end
+end
