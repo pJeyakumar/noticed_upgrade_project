73d0cfa475bc268bd8dbbe93f2c41d7c6927a587
pJeyakumar <jn.piranavan@gmail.com>
2024-06-05 12:16:17 -0400

    feat: permit email param for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :title, :company)
+      params.require(:user).permit(:first_name, :last_name, :email, :title, :company)
     end
 end
