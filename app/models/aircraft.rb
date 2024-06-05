fa2ae66994604e1ad9a07a86458112e5ad90bd96
pJeyakumar <jn.piranavan@gmail.com>
2024-06-05 11:43:55 -0400

    fix: indentation correction

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,12 +1,12 @@
 class Aircraft < ApplicationRecord
-    has_many :logbook_entries
+  has_many :logbook_entries
 
-    enum engine: {
-      single: 0,
-      multi: 1
-    }
-  
-    def to_s
-      make + " " + model
-    end
+  enum engine: {
+    single: 0,
+    multi: 1
+  }
+
+  def to_s
+    make + " " + model
+  end
 end
