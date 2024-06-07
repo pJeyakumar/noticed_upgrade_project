71f7501cdfbe0611e87443138ce32bbf8ebb4d9b
Stephen Beath <stephen@rcafinnovation.ca>
2024-06-07 14:06:33 -0400

    feat: first pass at tests and stubs for new aircraft notification

---
+++ spec/notifications/new_aircraft_created_notification_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe NewAircraftCreatedNotification, type: :model do
+
+  # Must eagerly create a user first, or notifications will not be created
+  let!(:user) { FactoryBot.create(:user) }
+
+  let!(:aircraft) { FactoryBot.create(:aircraft) }
+  
+  context 'when aircraft are created' do
+    it 'creates notifications' do
+      expect(aircraft).to be_valid
+      expect(user).to be_valid
+      expect(Notification.count).to be(1)
+
+      FactoryBot.create(:aircraft) 
+      expect(Notification.count).to be(2)
+    end
+
+    it 'notifies the correct user' do
+      FactoryBot.create(:user, first_name: "Sally")
+      FactoryBot.create(:aircraft)
+      expect(Notification.last.recipient.first_name).to eq("Sally")
+    end
+
+    # it 'uses the correct notification template'
+    #   # todo ...
+    # end
+  end
+end
