85f320da3103204243086ef552b0acc7598b05c3
Stephen Beath <stephen@rcafinnovation.ca>
2024-06-12 11:05:38 -0400

    fix: restored FactoryBot functionality

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
+require "support/factory_bot"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
@@ -30,11 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort(e.to_s.strip)
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = [
-    Rails.root.join("spec/fixtures")
-  ]
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
