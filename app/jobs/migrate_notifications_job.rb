52f937718ca060cd8f13472fde965751648cb5f8
pJeyakumar <jn.piranavan@gmail.com>
2024-06-24 20:43:58 +0000

    feat: add logs for info during process, after process, and if any errors have occurred, exception handling to safely catch errors and ensure the ApplicationJob continues

--- app/jobs/migrate_notifications_job.rb
+++ app/jobs/migrate_notifications_job.rb
@@ -9,10 +9,12 @@ class MigrateNotificationsJob < ApplicationJob
   class Notification < ApplicationRecord
     self.inheritance_column = nil
   end
-  
 
   def perform(*args)  
     total_processed = 0
+    total_noticed_events_created = 0
+    total_noticed_events_errored = 0
+
     benchmark_result = Benchmark.measure do
       last_processed_at = get_last_processed_at
 
@@ -20,12 +22,23 @@ class MigrateNotificationsJob < ApplicationJob
       Notification.order(created_at: :desc).where("created_at < ?", last_processed_at).limit(LIMIT).find_in_batches(order: :desc, batch_size: BATCH_SIZE) do |batch|
         batch.each do |notification|
           return if total_processed >= LIMIT
-          migrate_notification(notification)
-          total_processed += 1      
+          
+          begin
+            migrate_notification(notification)
+            total_processed += 1
+          rescue => e
+            total_noticed_events_errored += 1
+            Rails.logger.error("Error processing notification ID #{notification.id}: #{e.message}")
+          end
         end    
+        total_noticed_events_created += batch.size
+        Rails.logger.info("Completed processing batch. Total Noticed::Notifications created so far: #{total_noticed_events_created}")
       end
     end
+
     Rails.logger.info("Time taken to process notifications: #{benchmark_result.total} seconds")
+    Rails.logger.info("#{total_noticed_events_created} Noticed::Notifications created, #{total_noticed_events_errored} Noticed::Notifications errored, #{Noticed::Notification.count/Notification.count * 100}% of Notifications covered.")
+    puts "" # Included this so that the terminal will show the end of the Rails.logger.info (instead of the beginning and forcing you to scroll all the way down)
   end
 
   private
